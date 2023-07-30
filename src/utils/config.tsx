6371df028c41e332b7d08d7f2f693e82ec7c0ea8
Manav Kaushal <manavkaushal756@gmail.com>
2023-07-30 09:59:12 +0530

    initial setup complete

---
+++ src/utils/config.tsx
@@ -0,0 +1,30 @@
+const isProduction = process.env.NEXT_PUBLIC_NODE_ENV === "production";
+
+const appConfig = {
+  name: "Tiny Wheels",
+  logo: "/assets/images/logo-dark.png",
+  apiBaseUrl: isProduction ? "" : "http://localhost:8000/api",
+  websiteUrl: isProduction
+    ? "https://tinywheels.vercel.app"
+    : "http://localhost:3000",
+  fallbackImage: "/assets/images/placeholder.jpg",
+};
+
+const imageKitConfig = {
+  prefix: "https://ik.imagekit.io/tinywheels",
+};
+
+const defaultSEO = {
+  defaultTitle: appConfig.name,
+  titleTemplate: "%s | " + appConfig.name,
+  description: "",
+  canonical: "",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "",
+    site_name: appConfig.name,
+  },
+};
+
+export { appConfig, imageKitConfig, defaultSEO };
